8a029dcbde0abc1f2a63630f81dccb14615a838a
Garrett Rabian <garrett.rabian@coforma.io>
2026-09-14 13:29:33 -0400

    add back roles for now

--- services/shared/src/types/users.ts
+++ services/shared/src/types/users.ts
@@ -3,6 +3,8 @@ export enum UserRoles {
   HELP_DESK = "mdctdatasets-help-desk", // "MDCT Datasets Help Desk"
   INTERNAL = "mdctdatasets-internal-user", // "MDCT Datasets Internal User"
   STATE_USER = "mdctdatasets-state-user", // "MDCT Datasets State User",
+  APPROVER = "mdctdatasets-approver", // TODO: remove if unused
+  PROJECT_OFFICER = "mdctdatasets-project-officer", // TODO: remove if unused
 }
 
 export const isUserRole = (role: string): role is UserRoles => {
