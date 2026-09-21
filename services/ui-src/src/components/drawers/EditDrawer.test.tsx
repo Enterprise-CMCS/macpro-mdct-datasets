8288edc11d09b32039644a34908be71e70db4b82
ailZhou <aileen.zhou@coforma.io>
2026-09-21 18:02:56 -0400

    forgot prettier again

--- services/ui-src/src/components/drawers/EditDrawer.test.tsx
+++ services/ui-src/src/components/drawers/EditDrawer.test.tsx
@@ -20,7 +20,7 @@ describe("<EditDrawer />", () => {
         }}
         file={mockFile}
         onModalSubmit={() => mockOnSubmit()}
-      />,
+      />
     );
   });
   test("EditDrawer render", () => {
