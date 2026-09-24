6ee07f28a6503bbb171b861134f2de610d65578b
Garrett Rabian <garrett.rabian@coforma.io>
2026-09-24 10:14:44 -0400

    update db name to avoid conflict

--- deployment/stacks/data.ts
+++ deployment/stacks/data.ts
@@ -24,7 +24,7 @@ export function createDataComponents(props: CreateDataComponentsProps) {
     new DynamoDBTable(scope, "Uploads", {
       stage,
       isDev,
-      name: "uploads",
+      name: "datasets-uploads",
       partitionKey: {
         name: "state",
         type: dynamodb.AttributeType.STRING,
